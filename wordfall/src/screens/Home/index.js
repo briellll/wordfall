17875f52584bf898c776b361e8dd5850cfb99063
briellll <carloscgmorais@gmail.com>
2023-05-01 21:40:10 -0300

    colocando plano de fundo

--- wordfall/src/screens/Home/index.js
+++ wordfall/src/screens/Home/index.js
@@ -1,7 +1,9 @@
-import { StyleSheet, Text, View, StatusBar, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TouchableOpacity, ImageBackground } from 'react-native';
 
 export default function Home() {
   return (
+    <ImageBackground source={require('../../../assets/background.png')} style={styles.background}>
+         <View style={styles.overlay} />
     <View style={styles.container}>
       <Text style={styles.title}>wordfall</Text>
       <View style={styles.buttonsContainer}>
@@ -24,15 +26,29 @@ export default function Home() {
       </View>
       <StatusBar style="auto" />
     </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+    backgroundColor: '#FFFFFF'
+  },
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    backgroundColor: 'rgba(0,0,0,0.9)', // altere aqui para uma cor mais escura, se necessário
+    opacity: 0.5,
   },
   title: {
     backgroundColor: 'transparent',
@@ -40,7 +56,7 @@ const styles = StyleSheet.create({
     top: '12%',
     textAlign: 'center',
     width: '100%',
-    fontSize: 80,
+    fontSize: 90,
     fontWeight: 'bold',
     color: '#FAEBD7',
   },
@@ -49,7 +65,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     position: 'absolute',
-    bottom: '20%',
+    bottom: '15%',
     height: '18%',
   },
   button: {
