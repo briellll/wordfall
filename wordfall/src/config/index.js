6a4c5426b83fae374b33c09a53ae2e6559b6b237
briellll <carloscgmorais@gmail.com>
2023-05-28 20:32:01 -0300

    erro - autenticacao

--- wordfall/src/config/index.js
+++ wordfall/src/config/index.js
@@ -1,20 +1,21 @@
-import firebase from   'firebase/app';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-import 'firebase/firestore';
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCDUImEbz-tJ9Y1v6Sd49S27hyXLHzc8uY",
+  authDomain: "wordtyper-4272f.firebaseapp.com",
+  projectId: "wordtyper-4272f",
+  storageBucket: "wordtyper-4272f.appspot.com",
+  messagingSenderId: "254259945650",
+  appId: "1:254259945650:web:09fb59dcccffb8672d29ab"
+};
 
-var firebaseConfig = {
-    apiKey: "AIzaSyCDUImEbz-tJ9Y1v6Sd49S27hyXLHzc8uY",
-    authDomain: "wordtyper-4272f.firebaseapp.com",
-    projectId: "wordtyper-4272f",
-    storageBucket: "wordtyper-4272f.appspot.com",
-    messagingSenderId: "254259945650",
-    appId: "1:254259945650:web:09fb59dcccffb8672d29ab"
-  };
+// Initialize Firebase
+const Firebase = initializeApp(firebaseConfig);
 
-  // Initialize Firebase
-
-  if(firebase.app.lenght ===0){
-  Firebase = firebase.initializeApp(firebaseConfig);
-  }
-
-  export default Firebase;
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(Firebase);
--- wordfall/src/screens/Login/index.js
+++ wordfall/src/screens/Login/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { KeyboardAvoidingView, View, Text, TouchableOpacity, Image } from 'react-native';
-
 import styles from './styles';
 import logoIcon from '../../../assets/user.png';
+import Firebase from '../../config';
 
 
 export default function Login() {
+
   return (
     <>
       <KeyboardAvoidingView style={styles.container} behavior="padding">
