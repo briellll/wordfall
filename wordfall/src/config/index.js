5db8daef978f65ba58224d9ce02a92128508f67b
briellll <carloscgmorais@gmail.com>
2023-06-03 23:38:53 -0300

    autenticação e criação de usuario feita

--- wordfall/src/config/index.js
+++ wordfall/src/config/index.js
@@ -1,11 +1,11 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyCDUImEbz-tJ9Y1v6Sd49S27hyXLHzc8uY",
   authDomain: "wordtyper-4272f.firebaseapp.com",
   projectId: "wordtyper-4272f",
@@ -13,9 +13,3 @@ const firebaseConfig = {
   messagingSenderId: "254259945650",
   appId: "1:254259945650:web:09fb59dcccffb8672d29ab"
 };
-
-// Initialize Firebase
-const Firebase = initializeApp(firebaseConfig);
-
-// Initialize Firebase Authentication and get a reference to the service
-const auth = getAuth(Firebase);
--- wordfall/src/screens/Login/index.js
+++ wordfall/src/screens/Login/index.js
@@ -1,11 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import { KeyboardAvoidingView, View, Text, TouchableOpacity, Image } from 'react-native';
+import React, { useState } from 'react';
+import { KeyboardAvoidingView, View, Text, TouchableOpacity, Image, TextInput, Alert } from 'react-native';
 import styles from './styles';
 import logoIcon from '../../../assets/user.png';
-import Firebase from '../../config';
 
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from '../../config';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigation = useNavigation();
+
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+
+  const handleCreateAccount = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log('Conta Criada!');
+        const user = userCredential.user;
+        console.log(user);
+        navigation.navigate('Home');
+      })
+      .catch(error => {
+        console.log(error);
+        Alert.alert(error.message);
+      });
+  };
+
+  const handleSign = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log('Logado!');
+        const user = userCredential.user;
+        console.log(user);
+        navigation.navigate('Home');
+      })
+      .catch(error => {
+        console.log(error);
+        Alert.alert(error.message);
+      });
+  };
 
   return (
     <>
@@ -13,11 +50,26 @@ export default function Login() {
         <View style={styles.form}>
           <Image source={logoIcon} style={[styles.logo, { marginBottom: 20 }]} />
 
-          <TouchableOpacity style={styles.buttonSubmit}>
-            <Text style={styles.submitText}>Acessar com Google</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            onChangeText={(text) => setEmail(text)}
+            value={email}
+          />
+
+          <TextInput
+            style={styles.input}
+            placeholder="Senha"
+            secureTextEntry
+            onChangeText={(text) => setPassword(text)}
+            value={password}
+          />
+
+          <TouchableOpacity onPress={handleSign} style={styles.buttonSubmit}>
+            <Text style={styles.submitText}>Login</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity style={styles.buttonRegister}>
+          <TouchableOpacity onPress={handleCreateAccount} style={styles.buttonRegister}>
             <Text style={styles.registerText}>Criar conta gratuita</Text>
           </TouchableOpacity>
         </View>
